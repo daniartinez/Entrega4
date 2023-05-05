9df93bdfa9c16daf1c593e05ffeb386603f6c2a6
Daniela <martinez.danif@gmail.com>
2023-05-05 18:01:25 -0300

    Entrega4

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let nombre = prompt('Ingrese su nombre')
+
+let usuario = 'Bienvenida ' + nombre
+
+console.log(usuario)
+
+let n1 = Number(prompt('Escribe un número'))
+let n2 = Number(prompt('Escribe un segundo número'))
+let n3 = Number(prompt('Escribe un tercer número'))
+
+let resultadoOperacion = n1 + n2 + n3
+
+alert(resultadoOperacion)
